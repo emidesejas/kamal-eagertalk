bfe3599934942b06820791fd08137f96c7201bae
Emiliano <edesejas@eagerworks.com>
2025-06-24 13:06:01 -0300

    fix: client navigation on home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
 	component: App,
@@ -8,12 +8,12 @@ function App() {
 	return (
 		<div className="text-center">
 			<header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-				<a
-					href="/slides"
+				<Link
+					to="/slides"
 					className="hover:shadow-2xl hover:shadow-blue-500 rounded-xl border-2 p-4 transition-all font-mono hover:scale-105 hover:animate-[shadow-pulse_4s_ease-in-out_infinite]"
 				>
 					<h1>Kamal Eagertalk Slides</h1>
-				</a>
+				</Link>
 			</header>
 		</div>
 	);
